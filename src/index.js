18d54b24bd56579f7e67c53b3dd62157acbc4f8d
GabrielMioni <gabriel.mioni@gmail.com>
2018-04-26 11:18:53 -0700

    Added player piece initialization

--- src/index.js
+++ src/index.js
@@ -43,9 +43,36 @@ class ReactCheckers extends React.Component {
             }
         }
 
-        console.log(board);
+        board = this.initPlayers(board);
 
         return board;
+    }
+
+    initPlayers(board) {
+        const player1 = ['a8', 'c8', 'e8', 'g8', 'b7', 'd7', 'f7', 'h7', 'a6', 'c6', 'e6', 'g6',];
+        const player2 = ['b3', 'd3', 'f3', 'h3', 'a2', 'c2', 'e2', 'g2', 'b1', 'd1', 'f1', 'h1',];
+
+        let self = this;
+
+        player1.forEach(function (i) {
+            board[i] = self.createPiece(i, 'player1');
+        });
+
+        player2.forEach(function (i) {
+            board[i] = self.createPiece(i, 'player2');
+        });
+
+        return board;
+    }
+
+    createPiece(location, player) {
+        let piece = {};
+
+        piece.player   = player;
+        piece.location = location;
+        piece.isKing   = false;
+
+        return piece;
     }
 
     render() {
