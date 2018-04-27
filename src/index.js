302ffed8fabff00e13f70c37751f886274f82744
GabrielMioni <gabriel.mioni@gmail.com>
2018-04-27 16:30:56 -0700

    Rearranged methods in the ReactCheckers class

--- src/index.js
+++ src/index.js
@@ -23,7 +23,69 @@ class ReactCheckers extends React.Component {
         }
     }
 
+    setColumns() {
+        const columns = {};
+        columns.a = 0;
+        columns.b = 1;
+        columns.c = 2;
+        columns.d = 3;
+        columns.e = 4;
+        columns.f = 5;
+        columns.g = 6;
+        columns.h = 7;
+
+        return columns;
+    }
+
+    createBoard() {
+
+        let board = {};
+
+        for (let key in this.columns) {
+
+            if (this.columns.hasOwnProperty(key)) {
+                for (let n = 1; n <= 8 ; ++n) {
+
+                    let row = key + n;
+                    board[row] = null;
+                }
+            }
+        }
+
+        board = this.initPlayers(board);
+
+        return board;
+    }
+
+    initPlayers(board) {
+        const player1 = ['a8', 'c8', 'e8', 'g8', 'b7', 'd7', 'f7', 'h7', 'a6', 'c6', 'e6', 'g6',];
+        const player2 = ['b3', 'd3', 'f3', 'h3', 'a2', 'c2', 'e2', 'g2', 'b1', 'd1', 'f1', 'h1',];
+
+        let self = this;
+
+        player1.forEach(function (i) {
+            board[i] = self.createPiece(i, 'player1');
+        });
+
+        player2.forEach(function (i) {
+            board[i] = self.createPiece(i, 'player2');
+        });
+
+        return board;
+    }
+
+    createPiece(location, player) {
+        let piece = {};
+
+        piece.player   = player;
+        piece.location = location;
+        piece.isKing   = false;
+
+        return piece;
+    }
+
     getCurrentState() {
+
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         return history[history.length - 1];
     }
@@ -107,47 +169,6 @@ class ReactCheckers extends React.Component {
 
     }
 
-    handleClick(coordinates) {
-        const currentState = this.getCurrentState();
-        const boardState = currentState.boardState;
-        const clickedSquare = boardState[coordinates];
-
-        if (clickedSquare !== null) {
-
-            // Can't select opponents pieces
-            if (clickedSquare.player !== returnPlayerName(currentState.currentPlayer)) {
-                return;
-            }
-
-            // Unset active piece if it's clicked
-            if (this.state.activePiece === coordinates) {
-                this.setState({
-                    activePiece: null,
-                });
-                return;
-            }
-
-            // Set active piece
-            let movesData = this.getMoves(currentState, coordinates);
-
-            this.setState({
-                activePiece: coordinates,
-                moves: movesData[0],
-                jumpKills: movesData[1],
-            });
-
-            return;
-        }
-
-        if (this.state.activePiece === null) {
-            return;
-        }
-
-        if (this.state.moves.length > 0) {
-            this.movePiece(currentState, coordinates);
-        }
-    }
-
     movePiece(currentState, coordinates) {
         let boardState = currentState.boardState;
         let movingPiece = Object.assign({}, boardState[this.state.activePiece]);
@@ -207,65 +228,45 @@ class ReactCheckers extends React.Component {
         }
     }
 
-    setColumns() {
-        const columns = {};
-        columns.a = 0;
-        columns.b = 1;
-        columns.c = 2;
-        columns.d = 3;
-        columns.e = 4;
-        columns.f = 5;
-        columns.g = 6;
-        columns.h = 7;
+    handleClick(coordinates) {
+        const currentState = this.getCurrentState();
+        const boardState = currentState.boardState;
+        const clickedSquare = boardState[coordinates];
 
-        return columns;
-    }
+        if (clickedSquare !== null) {
 
-    createBoard() {
-
-        let board = {};
-
-        for (let key in this.columns) {
-
-            if (this.columns.hasOwnProperty(key)) {
-                for (let n = 1; n <= 8 ; ++n) {
-
-                    let row = key + n;
-                    board[row] = null;
-                }
+            // Can't select opponents pieces
+            if (clickedSquare.player !== returnPlayerName(currentState.currentPlayer)) {
+                return;
             }
+
+            // Unset active piece if it's clicked
+            if (this.state.activePiece === coordinates) {
+                this.setState({
+                    activePiece: null,
+                });
+                return;
+            }
+
+            // Set active piece
+            let movesData = this.getMoves(currentState, coordinates);
+
+            this.setState({
+                activePiece: coordinates,
+                moves: movesData[0],
+                jumpKills: movesData[1],
+            });
+
+            return;
         }
 
-        board = this.initPlayers(board);
+        if (this.state.activePiece === null) {
+            return;
+        }
 
-        return board;
-    }
-
-    initPlayers(board) {
-        const player1 = ['a8', 'c8', 'e8', 'g8', 'b7', 'd7', 'f7', 'h7', 'a6', 'c6', 'e6', 'g6',];
-        const player2 = ['b3', 'd3', 'f3', 'h3', 'a2', 'c2', 'e2', 'g2', 'b1', 'd1', 'f1', 'h1',];
-
-        let self = this;
-
-        player1.forEach(function (i) {
-            board[i] = self.createPiece(i, 'player1');
-        });
-
-        player2.forEach(function (i) {
-            board[i] = self.createPiece(i, 'player2');
-        });
-
-        return board;
-    }
-
-    createPiece(location, player) {
-        let piece = {};
-
-        piece.player   = player;
-        piece.location = location;
-        piece.isKing   = false;
-
-        return piece;
+        if (this.state.moves.length > 0) {
+            this.movePiece(currentState, coordinates);
+        }
     }
 
     render() {
