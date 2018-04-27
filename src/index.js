84c8662832ed86c8923b4146c3101eb2f6901ca7
GabrielMioni <gabriel.mioni@gmail.com>
2018-04-26 20:01:21 -0700

    Added piece movement

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class ReactCheckers extends React.Component {
                 boardState: this.createBoard(),
                 currentPlayer: true,
             }],
+            activePiece: null,
             moves: [],
             stepNumber: 0,
         }
@@ -24,9 +25,135 @@ class ReactCheckers extends React.Component {
         return history[history.length - 1];
     }
 
+    getCorners(coordinates) {
+
+        const col = getColAsInt(this.columns, coordinates);
+        const row = getRowAsInt(coordinates);
+
+        const columnLeft  = col -1 >= 0 ? getColAsAlph(this.columns, col - 1) : false;
+        const columnRight = col +1 <= 7 ? getColAsAlph(this.columns, col + 1) : false;
+
+        const rowUpper = row +1 < 9 ? row +1 : false;
+        const rowLower = row -1 > 0 ? row -1 : false;
+
+        let corners = {};
+
+        corners.leftUpper  = columnLeft  !== false && rowUpper !== false ? columnLeft  + rowUpper : null;
+        corners.rightUpper = columnRight !== false && rowUpper !== false ? columnRight + rowUpper : null;
+        corners.leftLower  = columnLeft  !== false && rowLower !== false ? columnLeft  + rowLower : null;
+        corners.rightLower = columnRight !== false && rowLower !== false ? columnRight + rowLower : null;
+
+        return corners;
+
+    }
+
+    getMoves(currentState, coordinates, isKing = false) {
+
+        // const self = this;
+
+        let moves = [];
+        let jumps = [];
+
+        let killJumps = {};
+
+        const corners = this.getCorners(coordinates);
+
+        const row = getRowAsInt(coordinates);
+        const advanceRow = returnPlayerName(this.state.currentPlayer) === 'player1' ? row+1 : row-1;
+        const boardState = currentState.boardState;
+
+        for (let key in corners) {
+            if (!corners.hasOwnProperty(key)) {
+                continue;
+            }
+
+            let cornerCoordinates = corners[key];
+
+            if (cornerCoordinates === null) {
+                continue;
+            }
+
+            if (!isKing && cornerCoordinates.indexOf(advanceRow)  < 0) {
+                continue;
+            }
+
+            if (boardState[cornerCoordinates] === null) {
+                moves.push(cornerCoordinates);
+            } else {
+                /* Do stuff if there's neighboring pieces */
+            }
+
+        }
+
+        let movesOut = moves.concat(jumps);
+
+        let killJumpsOut = killJumps.length > 0 ? killJumps : null;
+
+        return [movesOut, killJumpsOut];
+
+    }
+
     handleClick(coordinates) {
         const currentState = this.getCurrentState();
-        console.log(currentState);
+        const boardState = currentState.boardState;
+        const clickedSquare = boardState[coordinates];
+
+        // Can't click empty square if no active piece is set.
+        if (this.state.activePiece === null && clickedSquare === null) {
+            return;
+        }
+
+        if (this.state.activePiece !== null && this.state.moves.indexOf(coordinates)) {
+
+            let movingPiece = Object.assign({}, boardState[this.state.activePiece]);
+            boardState[this.state.activePiece] = null;
+            boardState[coordinates] = movingPiece;
+
+            /*TODO primitive movement. More to do here*/
+            this.setState({
+                history: this.state.history.concat([{
+                    boardState: boardState,
+                    currentPlayer: !this.state.history.currentPlayer,
+                }]),
+                activePiece: null,
+                moves: [],
+                stepNumber: this.state.history.length,
+            });
+
+            return;
+        }
+
+        // Clicking on the active piece unsets it
+        if (this.state.activePiece === coordinates) {
+            this.setState({
+                activePiece: null,
+            });
+            return;
+        }
+
+        // Can't click opponents pieces
+        if (clickedSquare.player !== returnPlayerName(currentState.currentPlayer)) {
+            return;
+        }
+
+        this.setState({
+            activePiece: coordinates,
+            moves: this.getMoves(currentState, coordinates),
+        });
+
+        /*
+        if (this.state.activePiece !== null && clickedSquare === null) {
+            this.setState({
+                moves: this.getMoves(currentState, coordinates),
+            });
+        }
+
+        /*
+         const moves = this.getMoves(currentState, coordinates);
+
+         console.log(moves);
+         */
+
     }
 
     setColumns() {
@@ -97,6 +224,8 @@ class ReactCheckers extends React.Component {
         const boardState = currentState.boardState;
         const currentPlayer = currentState.currentPlayer;
 
+        console.log(this.state);
+
         return(
             <Board
                 boardState = {boardState}
@@ -158,7 +287,7 @@ class Board extends React.Component {
 
             let playerMoveClass = currentPlayer + ('-move');
             let moveToClass = this.moves.indexOf(coordinates) > -1 ? 'movable ' + currentPlayer : '';
-            let colorClass  = ( isOdd(col) && isOdd(row) || (!isOdd(col) && !(isOdd(row)) ) ) ? 'white' : 'black';
+            let colorClass  = ( (isOdd(col) && isOdd(row)) || (!isOdd(col) && !(isOdd(row)) ) ) ? 'white' : 'black';
 
             let squareClasses = [];
 
@@ -199,6 +328,21 @@ function isOdd(n) {
 
 function getColAsInt(columns, coordinate) {
     return columns[coordinate.charAt(0)];
+}
+
+function getColAsAlph(columns, columnInt) {
+
+    for (let key in columns) {
+        if (!columns.hasOwnProperty(key)) {
+            continue;
+        }
+
+        if (columnInt === columns[key]) {
+            return key;
+        }
+    }
+
+    return false;
 }
 
 function getRowAsInt(coordinate) {
